37c01c7e6e0bb81ec16ed977fb9ea465f69990e9
Jeffrey Whewhetu <jeffwhewhetu@gmail.com>
2022-11-23 00:29:44 +0100

    Add an hello world program

---
+++ simple-interest/jeff.dart
@@ -0,0 +1,8 @@
+//I was suppose to write my simple interest code here but I've already done it and push to the repo...
+//So I would do dummy hello world program here...
+
+import 'dart:io';
+
+void main() {
+  print("Hello, World");
+}
